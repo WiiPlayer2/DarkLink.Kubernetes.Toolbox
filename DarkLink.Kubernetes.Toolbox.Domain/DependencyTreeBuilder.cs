9b8eff0704acb5cceebb3858f09652a5d41cddf2
CI <ci@home>
2025-06-08 12:41:44 +0200

    Allow filtering for relevant storage classes

--- DarkLink.Kubernetes.Toolbox.Domain/DependencyTreeBuilder.cs
+++ DarkLink.Kubernetes.Toolbox.Domain/DependencyTreeBuilder.cs
@@ -2,12 +2,13 @@ namespace DarkLink.Kubernetes.Toolbox.Domain;
 
 public static class DependencyTreeBuilder
 {
-    public static DependencyTree Build(Seq<Pod> pods, Seq<PersistentVolumeClaim> pvcs)
+    public static DependencyTree Build(Seq<Pod> pods, Seq<PersistentVolumeClaim> pvcs, Option<Seq<StorageClassName>> relevantStorageClassses = default)
     {
         return new(
             pods.ToDictionary(
                     x => x,
                     x => new PodDependency(pvcs
+                        .Where(y => relevantStorageClassses.Match(classes => classes.Contains(y.StorageClassName), () => true))
                         .Where(y => x.Volumes
                             .OfType<PodVolume.PersistentVolumeClaim_>()
                             .Any(z => z.Name == y.Metadata.Name && x.Metadata.Namespace == y.Metadata.Namespace))
--- DarkLink.Kubernetes.Toolbox.Tests/DependencyTreeBuilderTest.cs
+++ DarkLink.Kubernetes.Toolbox.Tests/DependencyTreeBuilderTest.cs
@@ -107,4 +107,31 @@ public class DependencyTreeBuilderTest
         // Assert
         result.Should().BeEquivalentTo(expected);
     }
+
+    [TestMethod]
+    public void Build_WithSinglePodReferencingOnlyOneRelevantPvc_ReturnsOnlyThisDependency()
+    {
+        // Arrange
+        var pods = Seq(
+            new Pod(new(ResourceName.From("pod"), ResourceNamespace.From("ns")))
+            {
+                Volumes =
+                [
+                    PodVolume.PersistentVolumeClaim(ResourceName.From("pvc")),
+                    PodVolume.PersistentVolumeClaim(ResourceName.From("pvc2")),
+                ],
+            },
+            new Pod(new(ResourceName.From("pod2"), ResourceNamespace.From("ns"))));
+        var pvcs = Seq(
+            new PersistentVolumeClaim(new(ResourceName.From("pvc"), ResourceNamespace.From("ns")), StorageClassName.From("nfs")),
+            new PersistentVolumeClaim(new(ResourceName.From("pvc2"), ResourceNamespace.From("ns")), StorageClassName.From("not-nfs")));
+        var relevantStorageClassses = Some(Seq1(StorageClassName.From("nfs")));
+        var expected = new DependencyTree(Map((pods.Head, new PodDependency(Seq1(NfsDependency.Pvc(pvcs.Head))))));
+        
+        // Act
+        var result = DependencyTreeBuilder.Build(pods, pvcs, relevantStorageClassses);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
 }
