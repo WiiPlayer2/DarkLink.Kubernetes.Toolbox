67894f29830ba829cc927d60606c9a7c142eef80
CI <ci@home>
2025-06-26 18:08:50 +0200

    Invoke auxiliary tool if not a parsed command

--- DarkLink.Kubernetes.Toolbox.Cli/Program.cs
+++ DarkLink.Kubernetes.Toolbox.Cli/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Net.Mime;
+using CliWrap;
 using DarkLink.Kubernetes.Toolbox.Application;
 using DarkLink.Kubernetes.Toolbox.Cli;
 using DarkLink.Kubernetes.Toolbox.Demo;
@@ -17,6 +20,7 @@ return await new CommandLineBuilder(new RootCommand()
         new DependenciesCommand(),
     })
     .UseDefaults()
+    .AddMiddleware(RunAuxCliCommand)
     .UseHost(
         args => new HostBuilder().ConfigureDefaults(args),
         hostBuilder => UseCommandHandlers<Runtime>(hostBuilder)
@@ -31,6 +35,30 @@ return await new CommandLineBuilder(new RootCommand()
 
 IHostBuilder UseCommandHandlers<RT>(IHostBuilder hostBuilder) where RT : struct, HasCancel<RT> => hostBuilder
     .UseCommandHandler<DependenciesCommand.NfsCommand, DependenciesNfsCommandHandler<RT>>();
+
+async Task RunAuxCliCommand(InvocationContext context, Func<InvocationContext, Task> next)
+{
+    if (context.ParseResult.Errors.Any() && context.ParseResult.Tokens.Any() && context.ParseResult.CommandResult.Command is RootCommand)
+    {
+        var tokens = context.ParseResult.Tokens;
+        var tool = tokens.First().Value;
+        var args = tokens
+            .Skip(1)
+            .Select(x => x.Value)
+            .ToArray();
+
+        var cliResult = await Cli.Wrap($"k8s-toolbox-{tool}")
+            .WithArguments(args)
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync(context.GetCancellationToken());
+        
+        context.ExitCode = cliResult.ExitCode;
+    }
+    else
+    {
+        await next(context);
+    }
+}
 
 void ConfigureServices<RT>(HostBuilderContext hostContext, IServiceCollection services)
     where RT : struct, HasCancel<RT>
